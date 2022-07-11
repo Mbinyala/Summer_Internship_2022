660e5131da0a85b7806668d50e022331ce86d009
Mbinyala <78340028+Mbinyala@users.noreply.github.com>
2022-07-12 01:07:31 +0530

    Create sort.js

---
+++ day6/sort.js
@@ -0,0 +1,9 @@
+const arr = new Array("Orange", "Kiwi", "Zucchini", "Banana");
+arr.sort();
+console.log(arr);
+
+// to sort in decending order
+
+const arr = new Array("Orange", "Kiwi", "Zucchini", "Banana");
+arr.reverse();
+console.log(arr);
