db0bb53c8181a9e8591a20d26eee3d9bb5104512
Mbinyala <78340028+Mbinyala@users.noreply.github.com>
2022-07-12 01:08:22 +0530

    Create swap.js

---
+++ day6/swap.js
@@ -0,0 +1,11 @@
+let a=9;
+let b=28;
+let temp;
+console.log("Before Swapping a is ",a);
+console.log("Before Swapping b is ",b);
+
+temp = a;
+a = b;
+b = temp;
+console.log("After Swapping a is ",a);
+console.log("After Swapping b is ",b);
